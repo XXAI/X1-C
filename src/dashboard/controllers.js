68e69ff5ef257e5e2a64095654d0dde6592d832c
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-10-21 15:09:28 -0500

    Se agrego información al dashboard

--- src/dashboard/controllers.js
+++ src/dashboard/controllers.js
@@ -8,6 +8,28 @@
         $scope.menuIsOpen = false;
         $scope.menu = Menu.getMenu();
         $scope.loggedUser = UsuarioData.getDatosUsuario();
+        $scope.actas = [];
+        $scope.actas_por_validar = 0;
+        $scope.cargando = true;
+
+        DashboardDataApi.cargarDatos(function(res){
+          for(var i in res.data.actas){
+            res.data.actas[i].fecha_importacion = new Date(res.data.actas[i].fecha_importacion);
+
+            var hoy = new Date();
+            var fecha = res.data.actas[i].fecha_importacion;
+            var dias = Math.floor((hoy - fecha) / (1000 * 3600 * 24));
+
+            res.data.actas[i].conteo_dias = dias;
+          }
+          $scope.actas = res.data.actas;
+          $scope.actas_por_validar = res.data.actas_sin_validar;
+          $scope.cargando = false;
+        },function(e){
+            Mensajero.mostrarToast({contenedor:'#modulo-contenedor',titulo:'Error:',mensaje:'Ocurrió un error al intentar obtener los datos.'});
+            $scope.cargando = false;
+            console.log(e);
+        });
 
         $scope.menuCerrado = !UsuarioData.obtenerEstadoMenu();
         if(!$scope.menuCerrado){
