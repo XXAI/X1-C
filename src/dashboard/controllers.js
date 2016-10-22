04dcc5969df687af003a635bbd9877a0398e4e53
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-10-21 20:25:21 -0500

    Agregado opcion para deshabilitar captura de actas

--- src/dashboard/controllers.js
+++ src/dashboard/controllers.js
@@ -2,14 +2,15 @@
 	'use strict';
     angular.module('DashboardModule')
     .controller('DashboardCtrl',
-    ['$rootScope', '$scope', 'DashboardDataApi', '$mdSidenav','$location','$mdBottomSheet','$timeout','Auth','Menu','UsuarioData',
-    function($rootScope, $scope, DashboardDataApi,$mdSidenav,$location,$mdBottomSheet,$timeout,Auth, Menu, UsuarioData){
+    ['$rootScope', '$scope', 'DashboardDataApi', '$mdSidenav','$location','$mdBottomSheet','$timeout','Auth','Menu','UsuarioData','Mensajero',
+    function($rootScope, $scope, DashboardDataApi,$mdSidenav,$location,$mdBottomSheet,$timeout,Auth, Menu, UsuarioData,Mensajero){
         $scope.menuSelected = $location.path();
         $scope.menuIsOpen = false;
         $scope.menu = Menu.getMenu();
         $scope.loggedUser = UsuarioData.getDatosUsuario();
         $scope.actas = [];
         $scope.actas_por_validar = 0;
+        $scope.actas_activas = 0;
         $scope.cargando = true;
 
         DashboardDataApi.cargarDatos(function(res){
@@ -24,12 +25,39 @@
           }
           $scope.actas = res.data.actas;
           $scope.actas_por_validar = res.data.actas_sin_validar;
+          $scope.actas_activas = res.data.actas_activas;
+
           $scope.cargando = false;
         },function(e){
             Mensajero.mostrarToast({contenedor:'#modulo-contenedor',titulo:'Error:',mensaje:'Ocurrió un error al intentar obtener los datos.'});
             $scope.cargando = false;
             console.log(e);
         });
+
+        $scope.configurarCaptura = function(){
+          $scope.cargando = true;
+          if($scope.actas_activas){
+            var estatus = 0;
+            $scope.actas_activas = 0;
+          }else{
+            var estatus = 1;
+            $scope.actas_activas = 1;
+          }
+
+          DashboardDataApi.configurarHabilitarCaptura(estatus,function(res){
+            var mensaje = 'Captura de Actas habilitada';
+            if(!$scope.actas_activas){
+              mensaje = 'Captura de Actas deshabilitada';
+            }
+            Mensajero.mostrarToast({contenedor:'#modulo-contenedor',mensaje:mensaje});
+            $scope.cargando = false;
+          },function(e){
+            $scope.actas_activas = !$scope.actas_activas;
+            Mensajero.mostrarToast({contenedor:'#modulo-contenedor',titulo:'Error:',mensaje:'Ocurrió un error al intentar configurar la aplicación.'});
+            $scope.cargando = false;
+            console.log(e);
+          });
+        };
 
         $scope.menuCerrado = !UsuarioData.obtenerEstadoMenu();
         if(!$scope.menuCerrado){
--- src/dashboard/services.js
+++ src/dashboard/services.js
@@ -5,6 +5,9 @@
 	       return {
 	        	cargarDatos: function (success, error) {
 	            	$http.get(URLS.BASE_API + '/dashboard').success(success).error(error)
+	           	},
+	           	configurarHabilitarCaptura: function(estatus,success,error){
+	           		$http.get(URLS.BASE_API + '/habilitar-captura-acta/'+estatus).success(success).error(error)
 	           	}
 	       };
 	   	}]);
